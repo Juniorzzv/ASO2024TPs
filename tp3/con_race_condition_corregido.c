a87585b32f6abf7832b852b55ec0d328cc96c2d7
Juniorzzv <167388853+Juniorzzv@users.noreply.github.com>
2024-05-29 00:33:45 -0300

    Update con_race_condition_corregido.c

--- tp3/con_race_condition_corregido.c
+++ tp3/con_race_condition_corregido.c
@@ -11,7 +11,7 @@ void *comer_hamburguesa(void *tid)
 	while (1 == 1)
 	{ 
 		while(turno!=(int)tid);
-    // INICIO DE LA ZONA CRÍTICA
+         // INICIO DE LA ZONA CRÍTICA
 		if (cantidad_restante_hamburguesas > 0)
 		{
 			printf("Hola! soy el hilo(comensal) %d , me voy a comer una hamburguesa ! ya que todavia queda/n %d \n", (int) tid, cantidad_restante_hamburguesas);
@@ -20,11 +20,11 @@ void *comer_hamburguesa(void *tid)
 		else
 		{
 			printf("SE TERMINARON LAS HAMBURGUESAS :( \n");
-
+                        turno = (turno + 1)% NUMBER_OF_THREADS;
 			pthread_exit(NULL); // forzar terminacion del hilo
 		}
-    // SALIDA DE LA ZONA CRÍTICA   
-    	turno = (turno + 1)% NUMBER_OF_THREADS;
+         // SALIDA DE LA ZONA CRÍTICA   
+    	       turno = (turno + 1)% NUMBER_OF_THREADS;
 	}
 }
 
